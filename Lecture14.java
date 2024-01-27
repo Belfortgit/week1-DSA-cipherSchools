ee5eb4eb0a008fc1c678fc17b60acc91e752c5a0
Belfortgit <rohitkeerthi101@gmail.com>
2024-01-27 18:57:03 +0530

    Multiple Inheritance through Interfaces

---
+++ Lecture14.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+interface A
+{
+    int var = 90;
+    void func();
+}
+interface B
+{
+    int var = 44;
+    void func();
+}
+
+interface C extends A,B    // Multiple inheritance
+{
+    int var = 83;
+    void func();
+}
+
+class Sample implements C
+{
+    public void func()
+    {
+        System.out.println("In class C");
+    }
+}
+
+class Lecture14
+{
+    public static void main(String t[])
+    {
+        Sample obj = new Sample();
+        obj.func();
+    }
+}
