65bf5fa7bb03b13eb67c24c6fc7c5902502ea65b
Belfortgit <rohitkeerthi101@gmail.com>
2024-01-27 10:45:37 +0530

    Final Keyword

---
+++ Lecture11.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class A
+{
+    final int a = 8; // Final values cannot be changed by child classes
+    final int b;
+    public A()
+    {
+        b = 36; // we can assing the value of final variable either in constructor or while declaration
+    }
+}
+
+final class B extends A  // final classes can't be inherited
+{
+    public void funcB()
+    {
+        System.out.println("Inside function B");
+    }
+}
+
+class Lecture11
+{
+    public static void main(String p[])
+    {
+        B obj = new B();
+        obj.funcB();
+    }
+}
